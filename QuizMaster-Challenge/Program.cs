c659b42a891f919df9146e647f5d2cd8338a3d56
MohammadFarwan <alfrwan58@gmail.com>
2024-06-09 22:59:55 +0300

    Lab 2 is completed

--- QuizMaster-Challenge/Program.cs
+++ QuizMaster-Challenge/Program.cs
@@ -1,10 +1,75 @@
-﻿namespace QuizMaster_Challenge
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizMaster_Challenge
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                StartQuiz();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program completed.");
+            }
+        }
+
+        public static void StartQuiz()
+        {
+            List<string> islamicQuestionsAndAnswers = new List<string>()
+            {
+                "Who is the final prophet in Islam?|Mohammad",
+                "What is the holy book of Islam?|Quran",
+                "What is the first pillar of Islam?|Shahada",
+                "What is the month of fasting in Islam?|Ramadan",
+                "What is the direction faced during Islamic prayer?|Qibla"
+            };
+
+            int sum = 0;
+            // Accessing Islamic questions and answers
+            foreach (string item in islamicQuestionsAndAnswers)
+            {
+                string[] parts = item.Split('|');
+                string question = parts[0];
+                string answer = parts[1];
+
+                Console.WriteLine("Question: " + question);
+                string ansInput = "";
+                bool validInput = false;
+                while (!validInput)
+                {
+                    ansInput = Console.ReadLine();
+                    if (ansInput.Trim().Length > 0)
+                    {
+                        validInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid answer.");
+                    }
+                }
+
+                if (answer.Equals(ansInput, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Correct!");
+                    sum++;
+                }
+                else
+                {
+                    Console.WriteLine("Your answer is wrong and the correct answer is : " + answer);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Your final result is {sum}/{islamicQuestionsAndAnswers.Count}");
+            Console.WriteLine();
         }
     }
 }
+
